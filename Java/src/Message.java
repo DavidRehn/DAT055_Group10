fd4bb0d236ec91d651bd408c64a9fd2858c03660
Iinguss <darian.damcc@gmail.com>
2026-02-11 17:16:06 +0100

    Update Message.java
    konstuktor på messege

--- Java/src/Message.java
+++ Java/src/Message.java
@@ -4,4 +4,20 @@ package src;
  */
 public class Message{
     // TODO
+    private int sender_id;
+    private int message_date;
+    private int message_id;
+    private int chat_id;
+    private string msg_type;
+    Private string content;
+
+    public Message ( int sender_id, int message_date, int message_id,  int chat_id,  string msg_type, string content){
+    this.sender_id =     sender_id;
+    this.message_date =  message_date;
+    this.message_id =    message_id;
+    this.chat_id =       chat_id;
+    this.msg_type =      msg_type;
+    this.content =       content;
+
+    }
 };
