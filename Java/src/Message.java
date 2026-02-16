e7e036e552fe4637ea3ef05da05de3ddfcf75125
David Rehn <davidrehn05@gmail.com>
2026-02-16 23:15:04 +0100

    Fixed syntax errors and added get-methods in message.java

--- Java/src/Message.java
+++ Java/src/Message.java
@@ -1,23 +1,39 @@
 package src;
+
+import java.time.LocalDateTime;
 /**
  * Describes the Message class of a chat system, contains a message, sender and time of creation
  */
 public class Message{
-    // TODO
     private int sender_id;
-    private int message_date;
+    private LocalDateTime timestamp;
     private int message_id;
     private int chat_id;
-    private string msg_type;
-    Private string content;
+    //private String msg_type;
+    //private String content;
 
-    public Message ( int sender_id, int message_date, int message_id,  int chat_id,  string msg_type, string content){
-    this.sender_id =     sender_id;
-    this.message_date =  message_date;
-    this.message_id =    message_id;
-    this.chat_id =       chat_id;
-    this.msg_type =      msg_type;
-    this.content =       content;
+    public Message(int sender_id, LocalDateTime timestamp, int message_id, int chat_id, String msg_type, String content){
+        this.sender_id = sender_id;
+        this.timestamp = timestamp;
+        this.message_id = message_id;
+        this.chat_id = chat_id;
+        //this.msg_type = msg_type;
+        //this.content = content;
+    }
 
+    public int GetSenderID(){
+        return sender_id;
+    }
+
+    public LocalDateTime GetTimestamp(){
+        return timestamp;
+    }
+
+    public int GetMessageID(){
+        return message_id;
+    }
+
+    public int GetChatID(){
+        return chat_id;
     }
 };
