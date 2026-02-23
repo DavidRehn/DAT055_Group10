d869be95138f8d129a8ba4279f577099da6ff6c3
jacras5 <jacob.rashid05@gmail.com>
2026-02-23 18:02:03 +0100

    Update GUI.java

--- Java/src/GUI.java
+++ Java/src/GUI.java
@@ -8,14 +8,12 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GUI extends JFrame {
-    private JFrame frame;
     private JPanel loginPanel, chatList, selectedChat;
     private JTextField username, password;
     public JButton loginButton, addChatButton;
 
 
     public GUI () {
-        frame = new JFrame();
         loginPanel = new JPanel();
         chatList = new JPanel();
         selectedChat = new JPanel();
@@ -26,12 +24,12 @@ public class GUI extends JFrame {
 
 
 
-        frame.setTitle("Chat program");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setSize(1650, 1080); //Sets the program to full screen by default
-        frame.setVisible(true);
-        frame.setResizable(false);
+        this.setTitle("Chat program");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(1650, 1080); //Sets the program to full screen by default
+        this.setVisible(true);
+        this.setResizable(false);
     }
 
     public void showLogInScreen() {
@@ -50,16 +48,17 @@ public class GUI extends JFrame {
         loginPanel.add(username);
         loginPanel.add(password);
         
-        frame.add(loginPanel);
-        frame.add(loginButton);
+        this.add(loginPanel);
+        this.add(loginButton);
     }
 
 
-    public void hideLogInScreen() {
-        loginPanel.setVisible(false);
-        username.setVisible(false);
-        password.setVisible(false);
-        loginButton.setVisible(false);
+
+    public void removeLogInScreen() {
+        this.remove(loginPanel);
+        this.remove(loginButton);
+        this.remove(username);
+        this.remove(password);
     }
 
 
@@ -67,15 +66,15 @@ public class GUI extends JFrame {
         chatList.setBounds(0, 100, 400, 1080);
         chatList.setBorder(BorderFactory.createLineBorder(Color.black));
         addChatButton.setBounds(0, 0, 400, 100);
-        frame.add(chatList);
-        frame.add(addChatButton);
+        this.add(chatList);
+        this.add(addChatButton);
     }
 
 
    public static void main(String[] args) { //används bara för test
         GUI gui = new GUI();
         gui.showLogInScreen();
-        gui.hideLogInScreen();
+        gui.removeLogInScreen();
         gui.showMainScreen();
     }
 }
