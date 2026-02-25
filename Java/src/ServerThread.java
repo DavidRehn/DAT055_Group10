32b67e4a467c87dd3f98a09b16243dda87082b22
David Rehn <davidrehn05@gmail.com>
2026-02-25 15:34:10 +0100

    updated serverthread

--- Java/src/ServerThread.java
+++ Java/src/ServerThread.java
@@ -1,28 +1,64 @@
 package src;
 import java.io.*;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ServerThread extends Thread{
     private ServerSocketChannel serverSocket;
-    private ClientHandler clientHandler;
+    private final ClientHandler clientHandler;
+    private final Selector selector;
 
-    public ServerThread(ServerSocketChannel serverSocket){
-        this.serverSocket = serverSocket;
+    public ServerThread() throws IOException{
         this.clientHandler = new ClientHandler();
+        selector = Selector.open();
     }
 
     @Override
     public void run(){
         SocketChannel clientSocket;
-        while (true) { 
+        Iterator<SelectionKey> iter = null;
+        Set<SelectionKey> selectedKeys;
+
+        // Starts the server
+        try {
+            serverSocket = ServerSocketChannel.open().bind(new InetSocketAddress(3333));
+            serverSocket.configureBlocking(false);
+            System.out.println("Server is running");
+        } catch (IOException e) {
+            System.out.println("Could not start server");
+        }
+
+        while(true) { 
             try {
-                clientSocket = serverSocket.accept();
-                clientHandler.AddClient(clientSocket);
-                System.out.println("Client connected");
+                selector.select();
+                selectedKeys = selector.selectedKeys();
+                iter = selectedKeys.iterator();
             } catch (IOException e) {
-                System.out.println("Client could not connect");
             }
+            while(iter.hasNext()){
+                SelectionKey key = iter.next();
+                iter.remove();
+
+                if(key.isAcceptable()){
+                    try {
+                        clientSocket = serverSocket.accept();
+                        clientHandler.AddClient(clientSocket);
+                        System.out.println("Client connected");
+                    }catch (IOException e) {
+                        System.out.println("Client could not connect");
+                    }
+                }else if (key.isReadable()){
+                    clientSocket = (SocketChannel) key.channel();
+
+                    // read from socket here
+                }
+            }
+            
+
+
+            
         }
     }
 }
--- Java/src/server.java
+++ Java/src/server.java
@@ -1,24 +1,17 @@
 package src;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.nio.channels.ServerSocketChannel;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class server{
     public static void main(String[] args) {
-        ServerSocketChannel serverSocket;
         ExecutorService threads = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
 
         try {
-            serverSocket = ServerSocketChannel.open().bind(new InetSocketAddress(3333));
-            ServerThread serverThread = new ServerThread(serverSocket);
+            ServerThread serverThread = new ServerThread();
             serverThread.start();
-            System.out.println("Server is running");
         } catch (IOException e) {
-            System.out.println("Could not start server");
         }
     }
 }
