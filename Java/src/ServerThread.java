498b70d149cfcf974d5bdd8ccceb26a14407021b
David Rehn <davidrehn05@gmail.com>
2026-02-26 12:07:33 +0100

    Added ability to send objects via non-blocking sockets

--- Java/src/ServerThread.java
+++ Java/src/ServerThread.java
@@ -1,6 +1,7 @@
 package src;
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
@@ -9,12 +10,10 @@ import java.util.concurrent.Executors;
 
 public class ServerThread extends Thread{
     private ServerSocketChannel serverSocket;
-    private final ClientHandler clientHandler;
     private final Selector selector;
     
 
     public ServerThread() throws IOException{
-        this.clientHandler = new ClientHandler();
         selector = Selector.open();
     }
 
@@ -29,6 +28,7 @@ public class ServerThread extends Thread{
         try {
             serverSocket = ServerSocketChannel.open().bind(new InetSocketAddress(3333));
             serverSocket.configureBlocking(false);
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
             System.out.println("Server is running");
         } catch (IOException e) {
             System.out.println("Could not start server");
@@ -41,28 +41,58 @@ public class ServerThread extends Thread{
                 iter = selectedKeys.iterator();
             } catch (IOException e) {
             }
-            while(iter.hasNext()){
+            while(iter.hasNext()){      // For all channels with new actions
                 SelectionKey key = iter.next();
                 iter.remove();
 
-                if(key.isAcceptable()){
-                    try {
-                        clientSocket = serverSocket.accept();
-                        clientHandler.AddClient(clientSocket);
-                        System.out.println("Client connected");
-                    }catch (IOException e) {
-                        System.out.println("Client could not connect");
-                    }
-                }else if (key.isReadable()){
-                    clientSocket = (SocketChannel) key.channel();
+                //if (!key.isValid()) continue; // skip cancelled keys
 
-                    // read from socket here
+                try {
+                    // Set up connection
+                    if(key.isAcceptable()){
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                        System.out.println("Client connected");
+                    } 
+                    // Read from channel
+                    else if(key.isReadable()){
+                        SocketChannel client = (SocketChannel) key.channel();
+                        try {
+                            Object obj = receiveObject(client);
+                            System.out.println((ChatUser) obj); // for testing  
+                        } catch (ClassNotFoundException e) {}
+                    }
+                } catch(IOException e){
+                    System.out.println("Client closed connection");
+                    key.cancel();
+                    try{ key.channel().close(); } catch(IOException a){}
                 }
             }
-            
+        }
+    }
 
+    private Object receiveObject(SocketChannel channel) throws IOException, ClassNotFoundException {
+        // Get length of object
+        ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+        while (lengthBuffer.hasRemaining()) {
+            channel.read(lengthBuffer);
+        }
+        lengthBuffer.flip();
+        int length = lengthBuffer.getInt();
 
-            
+        // Get object
+        ByteBuffer dataBuffer = ByteBuffer.allocate(length);
+        while (dataBuffer.hasRemaining()){
+            channel.read(dataBuffer);
+        }
+        dataBuffer.flip();
+        byte[] bytes = new byte[length];
+        dataBuffer.get(bytes);
+        try (ObjectInputStream objIn = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+            return objIn.readObject();
         }
     }
 }
+
--- Java/src/client.java
+++ Java/src/client.java
@@ -1,17 +1,52 @@
 package src;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 public class client {
     public static void main(String[] args) {
+        SocketChannel socketChannel = null;
         try {
-            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel = SocketChannel.open();
             socketChannel.connect(new InetSocketAddress("127.0.0.1", 3333));
             System.out.println("Connected to server");
         } catch (IOException e){
             System.out.println("Could not connect to server");
         }
+
+        
+
+
+        // for testing
+        try {
+            SendObject(socketChannel, new ChatUser("Ben", "ABC123"));
+            System.out.println("Object sent");
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        
+    }
+
+    private static void SendObject(SocketChannel channel, Object obj) throws IOException{
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream objOut = new ObjectOutputStream(byteStream)) {
+        objOut.writeObject(obj);
+        }
+
+        byte[] byteArray = byteStream.toByteArray();
+        ByteBuffer lenBuffer = ByteBuffer.allocate(4);
+        lenBuffer.putInt(byteArray.length);
+        lenBuffer.flip();
+        while (lenBuffer.hasRemaining()) {
+            channel.write(lenBuffer);
+	    }
+        ByteBuffer msgBuffer = ByteBuffer.wrap(byteArray);
+        while (msgBuffer.hasRemaining()) {
+	        channel.write(msgBuffer);
+	    }
     }
 }
