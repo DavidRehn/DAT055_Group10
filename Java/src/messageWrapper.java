073aa6885c9e2552bff395f243be596789be7f82
EoSDReiB <gabriel2004andersson@gmail.com>
2026-02-27 17:26:06 +0100

    Update messageWrapper.java

--- Java/src/messageWrapper.java
+++ Java/src/messageWrapper.java
@@ -1,7 +1,18 @@
 package src;
-
-public class messageWrapper extends Sendable{
-    public messageWrapper(String messageType, Object obj){
-        super(messageType, obj);
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+public class messageWrapper extends Sendable implements Serializable
+{
+    ArrayList<String> Chats;
+    messageWrapper(ArrayList<String> L){
+        super("UI");
+        Chats.addAll(L);
+    }
+    public Object getObject(){
+        return Chats;
+    }
+    public String toString(){
+        return Chats.toString();
     }
 }
