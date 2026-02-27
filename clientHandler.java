dfb752bd71a75b370b032e639a138fd10cbb39b0
DavidRehn <119331137+DavidRehn@users.noreply.github.com>
2026-02-27 14:35:11 +0100

    Add files via upload

---
+++ clientHandler.java
@@ -0,0 +1,94 @@
+package src;
+
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
+import java.nio.*;
+import java.net.Socket;
+
+
+public class clientHandler implements Runnable{
+    final private SocketChannel clientSocket;
+    final private SelectionKey sk;
+    public clientHandler(Selector s, SocketChannel c) throws IOException{
+        clientSocket = c;
+        c.configureBlocking(false);
+        
+        sk = clientSocket.register(s, 0);
+        sk.attach(this);
+        sk.interestOps(SelectionKey.OP_READ);
+        s.wakeup();
+    }
+    
+    @Override
+    public void run(){
+        
+        
+        try {
+            try {
+                Object request = receiveObject();
+                System.out.println("Received");
+                sendObject(new UserRequest("a"));
+				sk.interestOps(SelectionKey.OP_READ);
+            }catch (IOException e) {
+                sk.cancel();
+                System.out.println("Connection terminated");
+            }
+            
+            
+        } catch (Exception e) {
+            
+            System.out.println("Server could not print object: "+e.toString());
+        }
+    }
+    
+    
+    private void sendObject(Object obj) throws IOException{
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream objOut = new ObjectOutputStream(byteStream)) {
+        objOut.writeObject(obj);
+        }
+
+        byte[] byteArray = byteStream.toByteArray();
+        ByteBuffer lenBuffer = ByteBuffer.allocate(4);
+        lenBuffer.putInt(byteArray.length);
+        lenBuffer.flip();
+        while (lenBuffer.hasRemaining()) {
+            clientSocket.write(lenBuffer);
+        }
+        ByteBuffer msgBuffer = ByteBuffer.wrap(byteArray);
+        while (msgBuffer.hasRemaining()) {
+            clientSocket.write(msgBuffer);
+        }
+    }
+    
+    private Object receiveObject() throws IOException, ClassNotFoundException {
+        // Get length of object
+        ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+        while (lengthBuffer.hasRemaining()) {
+            if(clientSocket.read(lengthBuffer)==-1){
+			throw new EOFException("Connection closed early");
+			}
+        }
+        lengthBuffer.flip();
+        int length = lengthBuffer.getInt();
+
+        // Get object
+        ByteBuffer dataBuffer = ByteBuffer.allocate(length);
+        while (dataBuffer.hasRemaining()){
+            if(clientSocket.read(dataBuffer)==-1){
+			throw new EOFException("Data transfere incomplete");
+			}
+        }
+        dataBuffer.flip();
+        byte[] bytes = new byte[length];
+        dataBuffer.get(bytes);
+        try (ObjectInputStream objIn = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+            return objIn.readObject();
+        }
+        }
+    
+    
+    
+    
+}
---
+++ server.java
@@ -0,0 +1,77 @@
+package src;
+
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
+import java.nio.*;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class server{
+
+    public static void main(String[] args) {
+        ServerSocketChannel serverSocket = null;
+        SocketChannel clientSocket = null;
+        ExecutorService threads = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        
+        
+        Selector selector;
+        Set<SelectionKey> selectedKeys;
+        Iterator<SelectionKey> iter;
+        try {
+            serverSocket = ServerSocketChannel.open();
+            serverSocket.bind(new InetSocketAddress(3333));
+            serverSocket.configureBlocking(false);
+            
+            selector = Selector.open();
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+            
+            
+            System.out.println("Server is running");
+            while(true) { 
+                try {
+                    selector.select();
+                    selectedKeys = selector.selectedKeys();
+                    iter = selectedKeys.iterator();
+                    while(iter.hasNext()){
+                        SelectionKey key = iter.next();
+                        iter.remove();
+                        try {
+                            // Set up connection
+                            if(key.isAcceptable()){
+                                SocketChannel client = serverSocket.accept();
+                                new clientHandler(selector, client);
+                                System.out.println("Client connected");
+                            } 
+                            // Read from channel
+                            else if(key.isReadable()){
+                                
+                                key.interestOps(SelectionKey.OP_WRITE);
+                                System.out.println("Reading");
+                                threads.execute((clientHandler)key.attachment());
+                               
+                                }
+                            } catch(Exception e){
+                                System.out.println("Client closed connection");
+                                key.cancel();
+                                try{ key.channel().close(); } catch(IOException a){}
+                            }
+                            
+                            }
+                } catch (IOException e) {
+                    
+                    }
+                
+
+            }
+        } catch (IOException e) { 
+            System.out.println("Could not start server");
+        }
+        
+        
+    }
+}
