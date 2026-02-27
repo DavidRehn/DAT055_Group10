6c04c47af3f67f521a3d9ebc8f5ac464d3d8dd11
Iinguss <darian.damcc@gmail.com>
2026-02-27 16:24:14 +0100

    Update ButtonManagement.java Without client comunication

--- Java/src/ButtonManagement.java
+++ Java/src/ButtonManagement.java
@@ -1,5 +1,27 @@
 package src;
 
-public class ButtonManagement {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import java.util.*;
+//import java.swing.*; 
+
+public class ButtonManagement implements ActionListener  {
+    //private List<String> chatNames;// = new ArrayList<>();
+    private String[] chatJoinButtons;
+    //public ButtonManagement(){}
     
+    public void actionPerformed(ActionEvent e){
+        System.out.println("    in ButtonManagment");        
+        Object obj = e.getSource();
+        
+        
+        
+        if(obj instanceof JButton ){
+            JButton loginButton = (JButton)obj;
+            System.out.println("djdjdj "+e.getActionCommand());
+        }
+        
+    }
 }
