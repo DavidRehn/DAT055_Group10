bc8db58895bacd19ec799eb7a44211f33e19d743
David Rehn <davidrehn05@gmail.com>
2026-02-27 15:03:03 +0100

    Updated messageWrapper

--- Java/src/messageWrapper.java
+++ Java/src/messageWrapper.java
@@ -2,9 +2,14 @@ package src;
 
 public class messageWrapper implements Sendable{
     private String messageType;
+    private Object obj;
 
     @Override
     public String GetMessageType(){
         return messageType;
+    }
+
+    public Object GetObject(){
+        return obj;
     }
 }
