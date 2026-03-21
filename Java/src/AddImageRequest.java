2b5c2f630b8f4b9f23a79dc4358ed346329b7a57
EoSDReiB <gabriel2004andersson@gmail.com>
2026-03-21 16:55:54 +0100

    Update AddImageRequest.java

--- Java/src/AddImageRequest.java
+++ Java/src/AddImageRequest.java
@@ -2,7 +2,7 @@ package src;
 
 import src.Model.Entities.ImageMessage;
 
-public class AddImageRequest extends Sendable {
+public class AddImageRequest extends Sendable implements RunnableRequest{
     private ImageMessage message;
     private byte[] image;
     private String imageName;
@@ -35,5 +35,33 @@ public class AddImageRequest extends Sendable {
 
     public String GetChat() {
         return chat;
+    }
+    
+    public void runRequest(DataStorage D_CON, HandlerInterface h){
+        try {
+            ImageMessage m = (ImageMessage) this.getObject();
+            m.SetSender(h.getUser().getUserName());
+            byte[] img = (byte[]) this.GetImage();
+            String fileName = this.GetFileName();
+            ByteArrayInputStream b = new ByteArrayInputStream(img);
+            BufferedImage image = ImageIO.read(b);
+            File outputDir = new File(SERVER_IMAGE_DIR);
+            if (!outputDir.exists()) {
+                outputDir.mkdirs();
+                }
+            File outputFile = new File(outputDir, fileName + ".png");
+            ImageIO.write(image, "png", outputFile);
+            m.SetImagePath(CLIENT_IMAGE_DIR + fileName + ".png");
+            D_CON.AddMessage(m);
+            System.out.println("saved image");
+        
+            // Send back the image to be saved at client side (Should be done by observable)
+            ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+            ImageIO.write(image, "png", bOut);    // Automaticly converts all valid image formats to png (should already be png if sent from client)
+            byte[] imageBytes = bOut.toByteArray();
+            h.broadcastImage(this.GetChat(), imageBytes, fileName);
+            h.broadcastMessages(this.GetChat());
+            System.out.println("Sent image response");
+        } catch (IOException e) {}
     }
 }
