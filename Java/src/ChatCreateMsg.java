4a4b2174a2a2c1e101198d40fde2307f84696105
EoSDReiB <gabriel2004andersson@gmail.com>
2026-03-21 16:53:00 +0100

    Update ChatCreateMsg.java

--- Java/src/ChatCreateMsg.java
+++ Java/src/ChatCreateMsg.java
@@ -1,6 +1,6 @@
 package src;
 
-public class ChatCreateMsg extends Sendable {
+public class ChatCreateMsg extends Sendable implements RunnableRequest{
     private String chatTitle;
 
     public ChatCreateMsg(String title) {
@@ -11,5 +11,21 @@ public class ChatCreateMsg extends Sendable {
     @Override
     public Object getObject() {
         return chatTitle;
+    }
+    @Override
+    public void runRequest(DataStorage D_CON, HandlerInterface h){
+        try {
+        if (!D_CON.ChatExists((String) this.getObject())) {
+            try {
+                D_CON.AddChat(new GroupChat((String) this.getObject()));
+                D_CON.AddUserToChat((String) this.getObject(), h.getUser().getUserName());
+                h.broadcastChatsToUser();
+            } catch (RuntimeException ex) {
+                h.sendObject(new messageWrapper("Could not create chat.", "AUTH_FAIL"));
+                }
+        } else {
+            h.sendObject(new messageWrapper("Chat already exists", "AUTH_FAIL"));
+            }
+        } catch (IOException e) {}
     }
 }
